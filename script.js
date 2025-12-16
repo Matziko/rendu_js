aa27aa126277d185ee6b10b36832cd0bab3f92d3
Matziko <MATEOS.HOXHA@latoile.em-lyon.com>
2025-12-16 16:00:35 +0100

    .

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
 const API_URL = `https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/sneakers.json`;
 
-let nav=document.getElementById("nav");
-let header=document.getElementById("header");
-let main=document.getElementById("main");
-let avantages=document.getElementById("Avantages")
-let produits=document.getElementById("Produits")
-let services=document.getElementById("Services")
-let temoignages=document.getElementById("Temoignages")
+let nav = document.getElementById("nav");
+let header = document.getElementById("header");
+let main = document.getElementById("main");
+let avantages = document.getElementById("Avantages")
+let produits = document.getElementById("Produits")
+let services = document.getElementById("Services")
+let temoignages = document.getElementById("Temoignages")
 
 
 fetch(API_URL)
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    
-    
-    
-    
-    // data.produits.forEach(prod => {
-        //     dishes.innerHTML += `<div class="dishcard">
-        //     <img src="${plat.media.image}" alt="${plat.media.credit}" style="max-width: 300px;">
-        //   <p>Nom du plat : ${plat.name}</p>
-        //   <p>Pays : ${plat.country}</p>
-        //   <p>Origine : ${plat.origin.city}</p>
-        //   <p>Ingredients : </p>
-        //   ${ingredientsHtml}
-        //   <p>Temps de preparation : ${plat.serving.timeMin} min</p>
-        //   <p>Difficulté : ${etoile(plat.difficulty)}</p>
-        //   <p>Ce plat ${vegetarian(plat.diet.vegetarian)}</p>
-        //   <p>Ce plat est ${gluten(plat.diet.glutenFree)}</p>
-        //   </div>`;
-        // });
-    
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
 
-    nav.innerHTML=`
-    <h3>Logo</h3>
+
+        nav.innerHTML = `
+    <h2>Logo</h2>
     <ul>
     <li><a href="">Accueil</a></li>
     <li><a href="">Avantages</a></li>
@@ -42,41 +24,80 @@ fetch(API_URL)
     <li><a href="">Services</a></li>
     <li><a href="">Temoignages</a></li>
     </ul>
-    <button>Appeler</button>
+    <div>
+    <button>Appeler</button></div>
     `;
-    header.innerHTML=`
+        header.innerHTML = `
     <div class="hero-container">
     <h1>${data.nomCommercial}</h1>
     <p>${data.phraseAccroche}</p>
     <p>${data.texteAppelAction}</p>
-    <button>"Acheter"</button>
-    <button>"Explorer"</button>
+    <button class="button1">Acheter</button>
+    <button class="button2">Explorer</button>
     </div>
     `;
-    data.avantagesClients.forEach(element => {
-        resultavantages=`<p>${data.avantagesClients}</p>`;
-        return resultavantages;
-    });
-    avantages.innerHTML=`
+        data.avantagesClients.forEach(element => {
+            resultavantages = `<div class="avantage-container"><p>${data.avantagesClients}</p></div>`;
+            return resultavantages;
+        });
+        avantages.innerHTML = `
     <div class="container">
     <span>"Pourquoi"</span>
     <h2>Ce qui nous définit</h2>
     <p>Qualité sans égale dans chaque détail</p>
-    <div class="avantage-container">
-    ${resultavantages}
-    </div>
-    </div>`;
     
-    data.produits.forEach(element => {
-        console.log(data.produits["image-url"]);
-        // resultproduits = `<div class="card"><img src="${data.produits["image-url"]}"><h3>"${data.produits.nom}"</h3><p>"${data.produits.description}"</p></div>`;
+    ${resultavantages}
+    
+    </div>`;
 
-        return resultproduits;
-    });
+        let produitshtml = '';
+        data.produits.forEach(produit => {
+         produitshtml += `
+            <div class="card">
+            <img src="${produit["image-url"]} "style="max-width: 150px;">
+        <h3>${produit.nom}</h3>
+            <p>${produit.description}</p></div>`;
+        // return produitsHtml;
+        });
 
-    produits.innerHTML=`
+        produits.innerHTML = `
     <div class="container">
-    ${resultproduits}
+    ${produitshtml}
     </div>
     `;
-});
+
+            let serviceshtml = '';
+        data.produits.forEach(service => {
+         serviceshtml += `
+         <h3>${service.nom}</h3>
+         <p>${service.description}</p>
+         `;
+        });
+
+        services.innerHTML = `
+    <div class="container">
+    <h2>Vos sneakers, votre style</h2>
+    ${serviceshtml}
+    </div>
+    `;
+
+                let temohtml = '';
+        data.temoignages.forEach(temo => {
+         temohtml += `
+         <div class="avantage-container">
+         <p>${temo.note}/5</p>
+         <p>${temo.commentaire}</p>
+         <p>${temo.prenom}</p>
+         <p>${temo.typeExperience}</p>
+         </div>`;
+        });
+
+        temoignages.innerHTML = `
+    <div class="container">
+    <h2>Ils parlent</h2>
+    <p>Ce que nos clients pensent vraiment</p>
+    
+    ${temohtml}
+    </div>
+    `;
+    });
